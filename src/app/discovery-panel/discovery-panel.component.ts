ee040104c15886793bca4fd3c213875023512f80
fabianko <fabian.arismendi@usach.cl>
2021-01-21 23:09:15 -0300

    ADD: counter in case of hitting the pokemon and in case of failure@

--- src/app/discovery-panel/discovery-panel.component.ts
+++ src/app/discovery-panel/discovery-panel.component.ts
@@ -1,15 +1,31 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-
+import { ApiPokemonService } from '../api-pokemon.service';
 @Component({
   selector: 'app-discovery-panel',
   templateUrl: './discovery-panel.component.html',
-  styleUrls: ['./discovery-panel.component.css']
+  styleUrls: ['./discovery-panel.component.css'],
+  providers: [ApiPokemonService]
 })
 export class DiscoveryPanelComponent implements OnInit {
-
-  constructor() { }
+  listFound: any[] = [];
+  listWrong: any[] = [];
+  display_not_found: boolean;
+  keyValid: string;
+  constructor(public apiPokemonService: ApiPokemonService) { }
 
   ngOnInit(): void {
+  }
+  validPokemon() {
+    if (
+      this.keyValid === this.apiPokemonService.pokemonSelected.name || 
+      this.keyValid === this.apiPokemonService.pokemonSelected.id.toString()) {
+        console.log(this.apiPokemonService.pokemonSelected);
+        this.listFound.push(this.apiPokemonService.pokemonSelected);
+    }
+    else {
+      this.listWrong.push(this.apiPokemonService.pokemonSelected);
+    }
   }
 
 }
